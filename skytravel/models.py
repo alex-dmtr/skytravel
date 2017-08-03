044b55ae16146a5dcd3bf09b16b1cc6f34c9372c
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-08-03 18:42:02 +0300

    Add city ForeignKey to Event

--- skytravel/models.py
+++ skytravel/models.py
@@ -42,6 +42,7 @@ class Event(models.Model):
     link = models.URLField(default="/")
     description = models.CharField(max_length=500)
     date = models.DateTimeField(default=datetime.datetime.now())
+    city = models.ForeignKey(City, on_delete=models.CASCADE )
 
     def __str__(self):
         return self.name
